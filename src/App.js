bc720efb47b2b147512d2dbd6186bd257d113c8c
jessebillard <jessebillard@mac.com>
2018-10-04 16:31:19 -0400

    adds droppable component to column and provided droppableProps, innerRef function and placeholder

--- src/App.js
+++ src/App.js
@@ -2,18 +2,24 @@ import React, { Component } from 'react';
 import '@atlaskit/css-reset';
 import initialData from './initial-data';
 import Column from './column.js'
+import { DragDropContext } from 'react-beautiful-dnd'
 
 class App extends Component {
   state = initialData
+
+  onDragEnd = (result) => {
+    // todo : reorder our columns
+  }
+
   render() {
     return (
-      <div className="App">
+      <DragDropContext onDragEnd={this.onDragEnd} >
         {this.state.columnOrder.map(columnId => {
           const column = this.state.columns[columnId]
           const tasks = column.taskIds.map(taskId => this.state.tasks[taskId])
           return <Column key={column.id} column={column} tasks={tasks} />
         })}
-      </div>
+      </DragDropContext>      
     );
   }
 }
--- src/task.js
+++ src/task.js
@@ -1,11 +1,19 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+    border: 1px solid lightgrey;
+    border-radius: 2px;
+    padding: 8px;
+    margin-bottom: 8px;
+`
 
 class Task extends React.Component {
     render() {
         return (
-            <div>
-                {this.props.task.content}
-            </div>
+            <Container>
+                {this.props.task.content}            
+            </Container>
         )
     }
 }
