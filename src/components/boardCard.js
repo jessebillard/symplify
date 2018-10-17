18848d278f74550d3936c3cc177f7399e204f8d2
jessebillard <jessebillard@mac.com>
2018-10-16 22:51:23 -0400

    fixes styling of boardCard, mapsDispatch to prop for boardCard to update selectedBoard and adds ReactRouter Link to boardCard

--- src/components/boardCard.js
+++ src/components/boardCard.js
@@ -1,12 +1,29 @@
 import React from 'react'
+import { Card } from 'semantic-ui-react'
+import { Link } from 'react-router-dom'
+import { connect } from 'react-redux'
+import { selectedBoard } from '../actions/index'
 
-const BoardCard = ({board}) => {
+class BoardCard extends React.Component {
     
-    return (
-        <div className="col">
-            {board.title}
-        </div>        
-    )
+    handleClick = () => {
+        this.props.selectedBoard(this.props.board)
+    }
+
+    render() {
+        const { board } = this.props
+        return (
+            <div className="col">
+                <Link to={`board/${board.id}`}>
+                    <Card fluid raised={true} onClick={this.handleClick}>
+                        <Card.Content>
+                            <Card.Header>{board.title}</Card.Header>
+                        </Card.Content>
+                    </Card>
+                </Link>
+            </div>        
+        )
+    }
 }
 
-export default BoardCard
+export default connect(null , { selectedBoard })(BoardCard)
